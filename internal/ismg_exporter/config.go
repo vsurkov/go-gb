38faae2e29d06b2df59beb2c1890c7e117f53e26
vsurkov <surkovv@gmail.com>
2022-01-25 23:35:53 +0300

    Commit some files what was lost and last time

--- internal/ismg_exporter/config.go
+++ internal/ismg_exporter/config.go
@@ -1,4 +1,19 @@
 package ismgExporter
 
-type config struct {
+type Config struct {
+	Port                                         Port
+	DB_url, Jaeger_url, Sentry_url, Kafka_broker URL
+	App_id, App_key                              string
+}
+
+func NewConfig() *Config {
+	return &Config{
+		Port:         Port{0},
+		DB_url:       URL{""},
+		Jaeger_url:   URL{""},
+		Sentry_url:   URL{""},
+		Kafka_broker: URL{""},
+		App_id:       "",
+		App_key:      "",
+	}
 }
